84ebf7fd4c356f34d1268ec519f907be5cf9167f
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-25 14:48:44 +0200

    add e

--- api/bot.js
+++ api/bot.js
@@ -1,11 +1,69 @@
-const { Telegraf } = require("telegraf");
-
+import { Markup, Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import getWeatherByCity from '../index.js'
 const bot = new Telegraf('5301019579:AAGwSUWnD2-Yxa2o_no_glieXXp0mdFixD4');
 
 
+const testButton = Markup.keyboard([
+  Markup.button.callback('/test', '/test'),
+])
+
+const mainMenu = Markup.keyboard([
+  Markup.button.callback('/weather', '/weather'),
+  Markup.button.callback('/delete', '/delete')
+])
+
 bot.start((ctx) => ctx.reply('Welcome'));
 
-module.exports = async (request, response) => {
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+
+bot.command('menu', (ctx)=> {
+  ctx.reply('here is menu', mainMenu);
+})
+
+bot.command('hide', (ctx) => {
+  ctx.reply('removed', Markup.removeKeyboard());
+})
+
+bot.command('weather', async (ctx)=> {
+  currentScene = 'weather';
+  ctx.reply('Sure! What city do you want to check?');
+});
+
+
+bot.command('delete', async (ctx)=> {
+  // const messageId = ctx.message.message_id;
+  try {
+      if (previousMessageId) {
+          await ctx.deleteMessage(previousMessageId);
+          previousMessageId = null;
+      } else {
+          ctx.reply('previous message was deleted before');
+      }
+  } catch(err) {
+      console.log(err);
+  }
+});
+
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+
+bot.on(message('text'), async (ctx)=> {
+  switch(currentScene) {
+      case 'weather':
+          const res = await getWeatherByCity(ctx.message.text)
+          currentScene = 'main';
+          ctx.reply(res);
+          break;
+      default:
+          ctx.reply('I dont know what to do');
+  }
+  // console.log(ctx.message.from);
+  previousMessageId = ctx.message.message_id;
+  // ctx.telegram.sendMessage(ctx.message.chat.id, 'test message');
+  // ctx.reply('I dont know what to do', testButton);
+});
+
+const header = async (request, response) => {
   try {
     // Ensure that this is a message being sent
     if (request?.body) {
@@ -25,4 +83,4 @@ module.exports = async (request, response) => {
   response.send("OK");
 };
 
-export default handleMessage;
+export default header;
